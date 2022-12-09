f32840ed9354fdc9b6fc23f3e13792c504026586
JangHoseoung <argenhart123@gmail.com>
2022-12-09 14:41:21 +0900

    221209

--- c_work/homework/221209/0713.c
+++ c_work/homework/221209/0713.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int num1 = 0, i=1;
+    int total = 0;
+    int num = 1;
     printf("정수를 입력 하시오 : ");
-    scanf("%d", num1);
-    while(0<i<num1){
-        printf("%d+%d=%d\n",num1,i,num1+i);
+    scanf("%d", num);
+    while(){
+        
         i++;}
 return 0;
 }
