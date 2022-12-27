6756fba4f1bb1eca460708ea9e7457ebba2e17f0
JangHoseoung <argenhart123@gmail.com>
2022-12-27 16:07:44 +0900

    221227

--- c_work/dojeon/D22.c
+++ c_work/dojeon/D22.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+int main(void) {
+	int num, i = 0;
+	int arr[num];
 
-int main (){
-    int num = 0;
-    int num2 = num%2;
-    printf("정수를 입력 하시오 : ");
-    scanf("%d",&num);
-    if(num/2 == 1){
-        
-    }
-    
-    for(int i = 0;i<)
+	printf("10진수 정수 입력: ");
+	scanf("%d", &num);
+
+	while (num / 2 > 0) {
+		arr[i] = num%2; //나머지
+		num = num/2; //몫
+		i++;
+	}
+	
+    arr[i] = 1;
+
+	for (int j = i; j >= 0; j--) {
+		printf("%d", arr[j]);
+	}
+
 }
